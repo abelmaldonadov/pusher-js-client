39fda7a3e5e2f503400042b73320a6fff8cb3929
Abel Maldonado <abelmaldonadov@outlook.com>
2022-09-15 00:55:16 -0500

    Rock N Roll

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
+import Pusher from "pusher-js";
 
 function App() {
+  const [messages, setMessages] = useState([])
+  const [channelId, setChannelId] = useState('')
+  const [isSubscribed, setSubscriber] = useState(false)
+
+  useEffect(() => {
+    return () => {
+      const id = prompt("Ingrese su ID de canal")
+      subscribe(id)
+    }
+  }, [])
+
+  const subscribe = (id) => {
+    const pusher = new Pusher("2b64658bcea9de650324", {
+      cluster: "sa1",
+    })
+
+    // Liberar canal
+    pusher.unsubscribe(id)
+
+    // Subscribirse
+    const channel = pusher.subscribe(id);
+    channel.bind('send-message', function(data) {
+      alert(JSON.stringify(data))
+      setMessages([...messages, JSON.stringify(data)])
+    });
+    setChannelId(id)
+    setSubscriber(true)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h5 style={{color: isSubscribed?"green":"red"}}>{isSubscribed?"Subscrito":"No Subscrito"}</h5>
+      <h5>Id: {channelId}</h5>
+      <hr/>
+      {messages.map(item => <p>{item}</p>)}
     </div>
   );
 }
